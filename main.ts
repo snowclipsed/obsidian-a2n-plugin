c57d7825df0f64030bc59885822bc1ec1c0d2334
snowclipsed <harrybishnoi@gmail.com>
2024-06-24 21:19:52 -0400

    Added a function to read currently open file, changed plugin name in manifest

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
 // Remember to rename these classes and interfaces!
+import ollama from 'ollama';
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -14,19 +14,30 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('book', 'Anything2Notes', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			this.readCurrentFile();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
+		
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
+		
+
+		this.addCommand({
+			id: 'write-note',
+			name: 'Write a note from link',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				new SampleModal(this.app).open();
+			},
+			hotkeys: []
+		});
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -36,32 +47,12 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		this.addCommand({
+			id: 'process-link',
+			name: 'Process Provided Link',
+			callback: () => {
+
 			}
 		});
 
@@ -69,7 +60,7 @@ export default class MyPlugin extends Plugin {
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
+		// Using this function will automatically remove the event linkstener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
@@ -88,6 +79,48 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async readDoc(string: docPath) {
+		const fileContent = await this.app.vault.read(docPath);
+		return fileContent;
+	}
+
+	async readCurrentFile() {
+    const currentFile = this.app.workspace.getActiveFile();
+    
+    if (currentFile) {
+      try {
+        const content = await this.app.vault.read(currentFile);
+        
+        // Now 'content' contains the text of the current file
+        console.log('Current file content:', content);
+        
+        // You can perform operations with the content here
+        // For example, count words:
+        const wordCount = content.split(/\s+/).length;
+        new Notice(`Word count: ${wordCount}`);
+        
+        return content;
+      } catch (error) {
+        console.error('Error reading file:', error);
+        new Notice('Error reading current file');
+        return null;
+      }
+    } else {
+      new Notice('No file is currently open');
+      return null;
+    }
+  }
+
+	async saveDoc(string: docPath, content) {
+		await this.app.vault.modify(docPath, content);
+	}
+
+	async writeNotes(string:content){
+		const message = {role: 'user', content: "Hello, I am a user"};
+		const processedContent = await ollama.chat({ model: 'llama2', messages: [message], stream: true })
+t
 	}
 }
 
